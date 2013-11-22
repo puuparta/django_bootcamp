05cabd01e2189da6a0e9fd72dd66055046e069d4
P. Heinonen <pasi.heinonen@gmail.com>
2013-11-22 10:37:24 -0800

    article and articles plus sublime projectfile

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from article.models import Article
 
-# Register your models here.
+#enabloidaan auto admin
+admin.site.register(Article)
--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from article.models import Article
 #from django.template.loader import get_template
 #from django.template import Context
 
@@ -8,4 +9,14 @@ def hello(request):
    name=request.GET["k"]
    html="Moro %s. Mita kuuluu?" % name
    return render_to_response('hello.html',{'name':html})
+
+def articles(request):
+	return render_to_response('articles.html',
+								{'articles': Article.objects.all()})
+
+def article(request, article_id=1):
+	return render_to_response('article.html',
+								{'article' : Article.objects.get(id=article_id)})
+
+
 
