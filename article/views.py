1fc0a3565f90d78d4ff919b1daeee8c70ad88a2a
P. Heinonen <pasi.heinonen@gmail.com>
2013-11-23 05:46:47 -0800

    cookie and session handling sample snippet

--- article/views.py
+++ article/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+
+# session requires HttpResponse
 from django.http import HttpResponse
 from article.models import Article
 #from django.template.loader import get_template
@@ -11,12 +13,29 @@ def hello(request):
    return render_to_response('hello.html',{'name':html})
 
 def articles(request):
+	language = "en-gb"
+	session_language = "en-gb"
+	if 'lang' in request.COOKIES:
+		language = request.COOKIES["lang"]
+
+
+	if 'lang' in request.session:
+		session_language = request.session["lang"]
+
 	return render_to_response('articles.html',
-								{'articles': Article.objects.all()})
+								{'articles': Article.objects.all(),
+								  'language': language,
+								  'session_language': session_language})
 
 def article(request, article_id=1):
 	return render_to_response('article.html',
 								{'article' : Article.objects.get(id=article_id)})
+
+def language(request,language="en-gb"):
+	response = HttpResponse("setting language to %s" % language)
+	response.set_cookie("lang", language)
+	request.session["lang"] = language
+	return response
 
 
 
