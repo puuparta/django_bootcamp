b73a648f39d6501befc26b0cbb74d51fff18b2fb
P. Heinonen <pasi.heinonen@gmail.com>
2013-11-23 15:56:34 -0800

    init user auth urls + PEP8 fixes for lint

--- article/models.py
+++ article/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Article(models.Model):
-  title = models.CharField(max_length=200)
-  body = models.TextField()
-  pub_date = models.DateTimeField('date published')
-  likes = models.IntegerField()
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField()
---
+++ views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf    
